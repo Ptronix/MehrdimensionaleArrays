32b120fef5a7dd5efc9799109bbc2025f5911df1
Philipp.Schmidt <philipp-kevin-schmidt@web.de>
2018-08-23 11:40:13 +0200

    added 2d array

--- MehrdimensionaleArrays/Program.cs
+++ MehrdimensionaleArrays/Program.cs
@@ -8,8 +8,40 @@ namespace MehrdimensionaleArrays
 {
     class Program
     {
+        //Deklaration des arrays
+        private static bool[,] sitzPlatzPosition;
+
+        private static int platzNummer = 1;
+
         static void Main(string[] args)
-        {
+            //Sitzplaetze = 10 Reihen mit jeweils 20 Plaetzen
+        {   //[Y,X] Achse
+            //StandardWert bool=false
+            sitzPlatzPosition = new bool[10,20];
+
+            //SitzPlatz Reihe 5 Platz 15 besetzen = true
+            sitzPlatzPosition[5, 15] = true;
+
+            // Ausgabe der Platzbelegung
+
+            for (int reihe = 0; reihe < sitzPlatzPosition.GetLength(0); reihe++)
+            {
+                for (int platz = 0; platz < sitzPlatzPosition.GetLength(1); platz++)
+                {
+                    Console.Write(" " + platzNummer++ + " ");
+                    Console.Write(sitzPlatzPosition[reihe,platz]);
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+
+           
+
+            //instanziieren
+            
+
+
         }
     }
 }
